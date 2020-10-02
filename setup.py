897b977c5bffa6623de210497bf5863ac17de43a
Jeroen Franse <jeroen.franse@protonmail.com>
2020-10-02 16:11:34 +0200

    add myst header to resources

--- setup.py
+++ setup.py
@@ -10,7 +10,9 @@ setup(
    author='Jeroen Franse',
    author_email='contact@jeroenfranse.com',
    packages=['bookflow_utils'],  #same as name
-   install_requires=['mlflow', 'jupyter-book', 'click'], #external packages as dependencies
+   package_dir={'bookflow_utils': 'bookflow_utils'},
+   package_data={'bookflow_utils': ['resources/*']},
+   install_requires=['mlflow', 'jupyter-book', 'click', 'pyyaml'], #external packages as dependencies
    include_package_data=True,
    entry_points='''
         [console_scripts]
